a3397a5430daae7f42731fcc0dcf1ca7e855305e
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-12-12 01:04:24 +0500

    build1

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ mongoose
 	});
 
 // define a simple route
-app.get("/", (req, res) => {
-	res.json({
-		message:
-			"Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.",
-	});
-});
+// app.get("/", (req, res) => {
+// 	res.json({
+// 		message:
+// 			"Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.",
+// 	});
+// });
 
 require("./app/routes/note.routes.js")(app);
 require("./app/routes/market.routes.js")(app);
